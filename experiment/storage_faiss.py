50e5ad6c285fbaa9c429577480c49837fb6f59dc
Victor Lee <victorlee0505@gmail.com>
2023-06-15 11:17:49 -0400

    Chroma / FAISS experiment
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

---
+++ experiment/storage_faiss.py
@@ -0,0 +1,26 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import TextLoader
+
+
+loader = TextLoader("./data/doc/paul_graham_essay.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+docs = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings()
+
+db = FAISS.from_documents(docs, embeddings)
+
+retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 2}, max_tokens_limit=1000)
+
+query = "What did the author do growing up?"
+
+results = retriever.get_relevant_documents(query)
+
+i = 0
+for result in results:
+    print(f'index {i}: {result}')
+    i=i+1
+
