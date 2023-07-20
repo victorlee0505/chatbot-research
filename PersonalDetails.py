0ed7acc182a621e4de7a39db13f1ea57d10fe41d
Victor Lee <victorlee0505@gmail.com>
2023-07-20 09:38:04 -0400

    form builder WIP
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

---
+++ PersonalDetails.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+
+class PersonalDetails(BaseModel):
+    first_name: str = Field(
+        ...,
+        description="This is the first name of the user.",
+    )
+    last_name: str = Field(
+        ...,
+        description="This is the last name or surname of the user.",
+    )
+    full_name: str = Field(
+        ...,
+        description="Is the full name of the user ",
+    )
+    city: str = Field(
+        ...,
+        description="The name of the city where someone lives",
+    )
+    email: str = Field(
+        ...,
+        description="an email address that the person associates as theirs",
+    )
+    language: str = Field(
+        ..., enum=["spanish", "english", "french", "german", "italian"]
+    )
